e059f3afa32a986b9519c8e2223b93b49c819e34
unknown <aryaholmukhe@gmail.com>
2024-08-18 01:48:44 -0400

    updated mc_dropdown

--- client/app/mc_dropdown/page.tsx
+++ client/app/mc_dropdown/page.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import DropDown from '@/components/dropdownMenu_mc'
+import Navbar from '@/components/Navbar';
 
 const Page = () => {
   return (
-    <div className="p-12">
-        <DropDown options={[
-            'Knee Replacement Surgery',
-            'Others'
-        ]}/>
+    <div>
+        <Navbar />
+      <div className="p-5">
+        <DropDown question="Which surgery are you trying to recover from?" options={[
+          'Knee Replacement Surgery',
+          'Others'
+        ]} />
+
+        <h1 className='pt-24'>Recommended Exercises:</h1>
+        <DropDown question="Recommended Exercises:" options={[
+          'Straight Leg Raises',
+          'Ankle Pumps',
+          'Bed-Supported Knee Bends'
+        ]} />
+      </div>
     </div>
   )
 }
