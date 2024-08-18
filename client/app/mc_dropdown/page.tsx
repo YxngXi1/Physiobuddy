0bee4c23d61da078b35e0c32c26472ef525042f2
Yang <147933670+YxngXi1@users.noreply.github.com>
2024-08-18 10:05:17 -0400

    Update page.tsx

--- client/app/mc_dropdown/page.tsx
+++ client/app/mc_dropdown/page.tsx
@@ -7,22 +7,18 @@ const Page = () => {
   return (
     <div>
         <Navbar />
-      <div className="p-5">
-        <DropDown question="Which surgery are you trying to recover from?" options={[
-          'Knee Replacement Surgery',
-          'Others'
-        ]} />
-
-        <h1 className='pt-24'>Recommended Exercises:</h1>
-        <DropDown question="Recommended Exercises:" options={[
-          'Straight Leg Raises',
-          'Ankle Pumps',
-          'Bed-Supported Knee Bends'
-        ]} />
-      </div>
+      <div className="min-h-screen flex flex-col justify-center items-center gap-y-4">
+        <select 
+          className="bg-gray-200 p-2 rounded-full my-4"
+        >
+          <option value="1">Choose Options</option>
+          <option value="2">Knee Replacement Surgery</option>
+          <option value="3">Others</option>
+        </select>
         <Link href='/dashboard'>
-          <button className='bg-blue-500 rounded-full p-4'>next page</button>
+          <button className='bg-blue-500 rounded-full p-4 text-white'>Submit</button>
         </Link>
+      </div>
     </div>
   )
 }
