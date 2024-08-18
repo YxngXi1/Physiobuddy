5c16461fc4e9c1f515fef659c93d976442f66eec
unknown <aryaholmukhe@gmail.com>
2024-08-18 01:55:29 -0400

    updated ui

--- server/test.py
+++ server/test.py
@@ -1,36 +1,20 @@
-import cv2 as cv
-import mediapipe as mp
-import numpy as np
+import cv2
 
-mp_drawing= mp.solutions.drawing_utils
-mp_pose = mp.solutions.pose
+cap = cv2.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,480)
 
-cap = cv.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (640,480))
 
-# setting up mediapipe ruh roh
-with mp_pose.Pose(min_detection_confidence = 0.5, min_tracking_confidence = 0.5) as pose:
-    
-    while cap.isOpened():
-        ret, frame = cap.read()
-        
-        # recorloring to RGB
-        image = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-        image.flags.writeable = False
-        
-        # make the detection
-        results = pose.process(image)
-        
-        # recolroing back into the normal amazing spectacular BGR
-        image.flags.writeable = True
-        image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
-        
-        
-        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-        
-        cv.imshow("my mediapipe projet" , image)
-        
-        if cv.waitKey(10) & 0xFF == ord('q'):
-            break
-            
-    cap.release()
-    cv.destroyAllWindows()
+while(True):
+    ret, frame = cap.read()
+    out.write(frame)
+    cv2.imshow('frame', frame)
+    c = cv2.waitKey(1)
+    if c & 0xFF == ord('q'):
+        break
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
