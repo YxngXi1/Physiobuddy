14e0992841e9fcca65857d7a41afa9fd694a3ee0
Yang <147933670+YxngXi1@users.noreply.github.com>
2024-08-18 10:00:58 -0400

    Update page.tsx

--- client/app/exercises/page.tsx
+++ client/app/exercises/page.tsx
@@ -1,63 +1,30 @@
-'use client'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
 
-import Image from "next/image";
-import React, { useEffect, useState } from 'react';
-import Camera from '@/components/Camera'
-
-export default function Home() {
-  const [cameraEnabled, setCameraEnabled] = useState(false);
-  const [message, setMessage] = useState('Connecting to server...');
-  const [selectedExercise, setSelectedExercise] = useState('exercise1');
-
-  const toggleCamera = () => {
-    setCameraEnabled(!cameraEnabled);
-  }
-
-  const startRecording = () => {
-    fetch('http://localhost:8080/start_recording', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ exercise: selectedExercise })
-    })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error('Error:', error));
-  }
-
-  useEffect(() => {
-    fetch('http://localhost:8080/api/home')
-      .then(response => response.json())
-      .then(data => setMessage(data.message))
-      .catch(error => console.error('Error:', error));
-  }, []);
-
+const page = () => {
   return (
-    <main className="h-screen flex flex-col justify-center items-center">
-      <div className="my-4">
-          <h2>{message}</h2>
-        </div>
-      <div className="w-full h-full flex flex-col justify-center text-center">
-        <button onClick={toggleCamera} className="bg-blue-500 p-4 rounded-full">
-          {cameraEnabled ? 'Disable Camera' : 'Enable Camera'}
-        </button>
-        <select 
-          value={selectedExercise} 
-          onChange={(e) => setSelectedExercise(e.target.value)} 
-          className="bg-gray-200 p-2 rounded-full my-4"
-        >
-          <option value="exercise1">Exercise 1</option>
-          <option value="exercise2">Exercise 2</option>
-        </select>
-        <button className='bg-blue-500 p-4' onClick={startRecording}>Start Exercise</button>
-        
-        <div className="h-full w-full flex flex-col justify-center items-center relative">
-          <div className="h-4/6 w-4/6 absolute">
-            {cameraEnabled && <Camera />}
-          </div>
-        </div>
+    <div className='min-h-screen flex flex-col justify-center items-center gap-y-12'>
+      <h1 className='text-3xl font-semibold'>These are the exercises you may be performing today!</h1>
+      <div className='w-full flex justify-evenly border'>
+        <Image
+          src='/exercise1.png'
+          height={500}
+          width={500}
+          alt='exercise1'
+          />
+        <Image
+        src='/exercise2.png'
+        height={500}
+        width={500}
+        alt='exercise2'
+          />
       </div>
-    </main>
-  );
+      <Link href='/exercises'>
+          <button className='bg-blue-500 rounded-full p-4 text-white'>Start Your Workout</button>
+      </Link>
+    </div>
+  )
 }
+
+export default page
